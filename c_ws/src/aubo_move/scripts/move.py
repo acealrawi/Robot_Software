c53f57da9e602fc012d4656141e310d6016dd72f
Ali Alrawi <alialrawiu@gmail.com>
2019-01-29 16:45:34 +0100

    cleaned the code

--- c_ws/src/aubo_move/scripts/move.py
+++ c_ws/src/aubo_move/scripts/move.py
@@ -36,32 +36,66 @@ class MoveGroupPythonInteface(object):
         self.valveState = 0
         self.flipState = 0
         self.vacuumState = 0
-        self.valveValue = 0
         self.flipValue = 0
-        self.rate = rospy.Rate(1000) # 10hz
 
         rospy.spin()
+        
 
-    def home(self):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        group = self.group
-        joint_goal = group.get_current_joint_values()
-        joint_goal[0] = -0.001255
-        joint_goal[1] = -0.148822
-        joint_goal[2] = -1.406503
-        joint_goal[3] = 0.311441
-        joint_goal[4] = -1.571295
-        joint_goal[5] = -0.002450
+#*******************************MOVEMENT PLANNING**********************
+    def place(self, end):
+        waypoints = []
+        scale = 1
+        
+        end_x, end_y, end_z = self.coordination_decoder(end)
+        
+        wpose = self.group.get_current_pose().pose
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.z += scale * 0.3
+        waypoints.append(copy.deepcopy(wpose))
 
-        # The go command can be called with joint values, poses, or without any
-        # parameters if you have already set the pose or joint target for the group
-        group.go(joint_goal, wait=True)
+        wpose.position.x -= scale * end_x
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.z -= scale * end_z
+        waypoints.append(copy.deepcopy(wpose))
 
-        # Calling ``stop()`` ensures that there is no residual movement
-        group.stop()
-        current_joints = self.group.get_current_joint_values()
+        (plan, fraction) = self.group.compute_cartesian_path(waypoints, 0.01, 0.0)
+
+        self.execute_plan(plan)
+        self.publish_valve_value(0)
+
+        while self.valveState == 1:
+            print "wating for the valve to turn off"
+            # self.valveState = 0
+            time.sleep(1)
+
+    def pick(self, start):
+        waypoints = []
+        start_x, start_y, start_z = self.coordination_decoder(start)
+        
+        wpose = geometry_msgs.msg.Pose()
+        q = tf.transformations.quaternion_from_euler(3.14,0,-1.57)
+        
+        wpose.position.x = -start_x
+        wpose.position.y = start_y
+        wpose.position.z = start_z
+
+        wpose.orientation.x = q[0]
+        wpose.orientation.y = q[1]
+        wpose.orientation.z = q[2]
+        wpose.orientation.w = q[3]
+        waypoints.append(copy.deepcopy(wpose))
+
+        (plan, fraction) = self.group.compute_cartesian_path(waypoints, 0.01, 0.0)
+
+        self.execute_plan(plan)
+
+        self.publish_valve_value(1)
+        while self.valveState == 0:
+            print "wating for the valve to turn on"
+            # self.valveState = 1
+            time.sleep(1)
 
     def go_to_pose_goal(self, start, end):
         # Copy class variables to local variables to make the web tutorials more clear.
@@ -141,7 +175,6 @@ class MoveGroupPythonInteface(object):
         # waypoints.append(copy.deepcopy(wpose))
 
         self.publish_valve_value(1)
-        # self.valveValue = 1
         while self.valveState == 0:
             print "wating for the valve to turn on"
             time.sleep(20)
@@ -158,7 +191,6 @@ class MoveGroupPythonInteface(object):
 
         
         self.publish_valve_value(0)
-        # self.valveValue = 0
 
         while self.valveState == 1:
             print "wating for the valve to turn off"
@@ -175,6 +207,8 @@ class MoveGroupPythonInteface(object):
                                         0.01,        # eef_step
                                         0.0)         # jump_threshold
         return plan, fraction
+
+#*******************************MOVEMENT EXECUTION*********************
     def plan_and_execute(self, start, end, scale=1):
         print "home"
         self.home()
@@ -182,67 +216,61 @@ class MoveGroupPythonInteface(object):
         plan, fraction = self.plan_cartesian_path(start, end, scale)
         self.execute_plan(plan)
 
-    def pick(self, start):
-        waypoints = []
-        start_x, start_y, start_z = self.coordination_decoder(start)
-        
-        wpose = geometry_msgs.msg.Pose()
-        q = tf.transformations.quaternion_from_euler(3.14,0,-1.57)
-        
-        wpose.position.x = -start_x
-        wpose.position.y = start_y
-        wpose.position.z = start_z
+    def execute_plan(self, plan):
+        print "executing"
+        self.group.execute(plan, wait=True)
+    def execute_pick_and_place(self, motion_message):
+        self.home()
+        start, end = self.motion_decoder(motion_message)
+        self.pick(start)
+        self.place(end)
+        self.home()
 
-        wpose.orientation.x = q[0]
-        wpose.orientation.y = q[1]
-        wpose.orientation.z = q[2]
-        wpose.orientation.w = q[3]
-        waypoints.append(copy.deepcopy(wpose))
+    def home(self):
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        group = self.group
+        joint_goal = group.get_current_joint_values()
+        joint_goal[0] = -0.001255
+        joint_goal[1] = -0.148822
+        joint_goal[2] = -1.406503
+        joint_goal[3] = 0.311441
+        joint_goal[4] = -1.571295
+        joint_goal[5] = -0.002450
 
-        (plan, fraction) = self.group.compute_cartesian_path(waypoints, 0.01, 0.0)
+        # The go command can be called with joint values, poses, or without any
+        # parameters if you have already set the pose or joint target for the group
+        group.go(joint_goal, wait=True)
+        group.stop()
 
-        self.execute_plan(plan)
+#*******************************PUBLISHERS*****************************
+    def publish_valve_value(self, value):
+        self.gripper_server_publisher.publish("valveValue:{0}".format(value))
+    def publish_flip_value(self, value):
+        self.gripper_server_publisher.publish("flipValue:{0}".format(value))
 
-        self.publish_valve_value(1)
-        while self.valveState == 0:
-            print "wating for the valve to turn on"
-            # self.valveState = 1
-            time.sleep(1)
+#*******************************UTILITIES******************************
+    def motion_decoder(self, motion):
+        message = motion.split(' ')
+        start = message[1]
+        end = message[2]
+        return start, end
 
+    def coordination_decoder(self, coordination):
+        x,y,z = coordination.split(',')
+        return float(x.strip()),float(y.strip()),float(z.strip())
 
-    def place(self, end):
-        waypoints = []
-        scale = 1
-        
-        end_x, end_y, end_z = self.coordination_decoder(end)
-        
-        wpose = self.group.get_current_pose().pose
-        waypoints.append(copy.deepcopy(wpose))
-        
-        wpose.position.z += scale * 0.3
-        waypoints.append(copy.deepcopy(wpose))
+    def reverse_digital_value(self, value):
+        if value == 1:
+            return 0
+        else:
+            return 1
 
-        wpose.position.x -= scale * end_x
-        waypoints.append(copy.deepcopy(wpose))
-        
-        wpose.position.z -= scale * end_z
-        waypoints.append(copy.deepcopy(wpose))
+    def key_value_pair_decoder(self, message):
+        state = int(message.split(':')[1].strip())
+        return state
 
-        (plan, fraction) = self.group.compute_cartesian_path(waypoints, 0.01, 0.0)
-
-        self.execute_plan(plan)
-        self.publish_valve_value(0)
-
-        while self.valveState == 1:
-            print "wating for the valve to turn off"
-            # self.valveState = 0
-            time.sleep(1)
-        
-
-                 
-        
-        
-    
     def display_trajectory(self, plan):
         robot = self.robot
         display_trajectory_publisher = self.display_trajectory_publisher
@@ -251,64 +279,6 @@ class MoveGroupPythonInteface(object):
         display_trajectory.trajectory.append(plan)
         # Publish
         display_trajectory_publisher.publish(display_trajectory);
-
-    def execute_plan(self, plan):
-        print "executing"
-        self.group.execute(plan, wait=True)
-
-    
-    def motion_decoder(self, motion):
-        message = motion.split(' ')
-        start = message[1]
-        end = message[2]
-        print start
-        print end
-        return start, end
-
-    def coordination_decoder(self, coordination):
-        x,y,z = coordination.split(',')
-        return float(x.strip()),float(y.strip()),float(z.strip())
-
-    def handle_gui_callback(self, data):
-        request = data.data
-        print(request)
-        if request.startswith("motion"):
-            thread = threading.Thread(target=self.execute_pick_and_place, args=(request,))
-            thread.start()
-        # if request.startswith("stop"):
-            
-        if request.startswith("flip"):
-            self.gripper_server_publisher.publish("flipValue:1")
-            
-        if request.startswith("home"):
-            self.home()
-            
-        if request.startswith("valve"):
-            value = self.gripper_message_decoder(request)
-            publish_valve_value(value)
-            
-
-    def handle_gripper_callback(self, data):
-        request = data.data
-        if request.startswith("valveState"):
-            self.handle_valve_message(request)
-        if request.startswith("flipState"):
-            self.handle_flip_message(request)
-        if request.startswith("vacuumState"):
-            self.handle_vacuum_message(request)
-
-    def gripper_message_decoder(self, message):
-        state = int(message.split(':')[1].strip())
-        return state
-
-    def handle_valve_message(self, state):
-        self.valveState = self.gripper_message_decoder(state)
-
-    def handle_flip_message(self, state):
-        self.flipState = self.gripper_message_decoder(state)
-
-    def handle_vacuum_message(self, state):
-        self.vacuumState = self.gripper_message_decoder(state)
 
     def print_information(self):
         # We can get the name of the reference frame for this robot:
@@ -325,17 +295,59 @@ class MoveGroupPythonInteface(object):
         print "============ Printing robot state"
         print self.robot.get_current_state()
         print ""
-    
-    def publish_valve_value(self, value):
-        self.gripper_server_publisher.publish("valveValue:{0}".format(value))
 
-    def execute_pick_and_place(self, motion_message):
-        self.home()
-        start, end = self.motion_decoder(motion_message)
-        self.pick(start)
-        self.place(end)
-        self.home()
-        
+#*******************************COMMANDS*******************************
+    def handle_motion_command(self, request):
+        thread = threading.Thread(target=self.execute_pick_and_place, args=(request,))
+        thread.start()
+
+    def handle_flip_command(self):
+        flipValue = self.reverse_digital_value(self.flipState):
+        self.publish_flip_value(flipValue)
+
+    def handle_valve_command(self):
+        valveValue = self.reverse_digital_value(self.valveState):
+        self.publish_valve_value(valveValue)
+
+#*******************************MESSAGES*******************************
+    def handle_valve_message(self, state):
+        self.valveState = self.key_value_pair_decoder(state)
+
+    def handle_flip_message(self, state):
+        self.flipState = self.key_value_pair_decoder(state)
+
+    def handle_vacuum_message(self, state):
+        self.vacuumState = self.key_value_pair_decoder(state)
+
+#*******************************CALLBACKS******************************
+    def handle_gui_callback(self, data):
+        request = data.data
+        if request.startswith("motion"):
+            self.handle_motion_command(request)
+        # if request.startswith("stop"):
+            
+        if request.startswith("flip"):
+            self.handle_flip_command()
+            
+        if request.startswith("home"):
+            self.home()
+            
+        if request.startswith("valve"):
+            self.handle_valve_command()
+
+    def handle_gripper_callback(self, data):
+        request = data.data
+        if request.startswith("valveState"):
+            self.handle_valve_message(request)
+
+        if request.startswith("flipState"):
+            self.handle_flip_message(request)
+
+        if request.startswith("vacuumState"):
+            self.handle_vacuum_message(request)
+
+
+#*******************************TESTS**********************************
     def test_run(self):
         print "**********STARTING TEST RUN**********"
         self.home()
@@ -345,11 +357,6 @@ class MoveGroupPythonInteface(object):
         self.place(end)
         self.home()
         print "**********TEST RUN END**********"
-        
-
-    
-
-
 
 def main():
   try:
